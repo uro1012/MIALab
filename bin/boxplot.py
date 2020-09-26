729a9a71adf37b5e083482f38652e628e017daff
Cyril Albrecht <kwonltp93@gmail.com>
2020-09-26 10:42:07 +0200

    added boxplots for results evaluation

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def main():
@@ -9,7 +10,31 @@ def main():
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
     # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
-    pass  # pass is just a placeholder if there is no other code
+
+    labels = ['Amygdala', 'GreyMatter', 'Hippocampus', 'Thalamus', 'WhiteMatter']
+    path = "mia-result/2020-09-26-02-54-03/results.csv"
+    results = pd.read_csv(path, sep=';')
+
+    shape = (len(labels), int(len(results)/len(labels)))
+    dice = np.zeros(shape)
+    hdrfdst = np.zeros(shape)
+    for i in range(len(labels)):
+        dice[i] = results.loc[results['LABEL'] == labels[i]]['DICE'].values.tolist()
+        hdrfdst[i] = results.loc[results['LABEL'] == labels[i]]['HDRFDST'].values.tolist()
+
+        plt.subplot(2, len(labels), i+1)
+        plt.boxplot(dice[i])
+        plt.title(labels[i])
+        if i == 0:
+            plt.ylabel('Dice')
+
+        plt.subplot(2, len(labels), i+1+len(labels))
+        plt.boxplot(hdrfdst[i])
+        plt.title(labels[i])
+        if i == 0:
+            plt.ylabel('Hausdorff')
+
+    plt.show()
 
 
 if __name__ == '__main__':
